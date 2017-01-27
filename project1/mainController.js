416872c91a760d6289022d43526d4273b25dc833
Daniel <danielalegria1217@gmail.com>
2017-01-26 18:02:31 -0700

    project1

---
+++ project1/mainController.js
@@ -0,0 +1,34 @@
+angular.module("app")
+.controller("mainController", function($scope, $http, mainService, weatherService){
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+  $scope.date = new Date();
+
+  weatherService.getWeather().then(function(response){
+    console.log(response);
+    console.log(response.data.query.results.channel);
+    console.log(response.data.query.results.channel.item.condition.code)
+    $scope.weather = response.data.query.results.channel;
+    var weatherCode = response.data.query.results.channel.item.condition.code;
+    $scope.icon = weatherService.iconFunction(weatherCode);
+
+  });
+
+  mainService.getData().success(function(response){
+    $scope.data = response.data.children;
+    console.log(response.data.children);
+  });
+
+  $scope.getSmmry = function(item){
+    if (!item.smmry) {
+      mainService.getSmmry(item.data.url).then(function(response){
+        item.smmry = response.data.sm_api_content;
+        console.log(response.data.sm_api_content);
+      });
+    }
+  }
+
+});
